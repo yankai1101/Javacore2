b3cabbee069ee3e7da5b4a5e1455d927027ecfb0
yankai13 <yankai@lunaon.com>
2020-03-24 15:47:27 +0800

    创建文件： 下游收集器 并行流测试

---
+++ vip.abatt.unit01/DownstreamCollectors.java
@@ -0,0 +1,7 @@
+/**
+ * Author:YANKAI_1101
+ * Date:2020/3/24
+ * Desc：TODO 下游收集器
+ **/
+public class DownstreamCollectors {
+}
---
+++ vip.abatt.unit01/ParallelStream.java
@@ -0,0 +1,7 @@
+/**
+ * Author:YANKAI_1101
+ * Date:2020/3/24
+ * Desc：TODO 并行流测试
+ **/
+public class ParallelStream {
+}
